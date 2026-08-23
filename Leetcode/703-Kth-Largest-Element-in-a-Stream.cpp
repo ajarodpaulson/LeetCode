afa7470814fe276d222117cac3ee4843eba93f05
Jarod Paulson <ajarodpaulson.ubc@gmail.com>
2026-08-23 11:18:01 -0700

    [703] [Time Beats: 100.00%] [Memory Beats: 25.97%] - LeetPush

---
+++ Leetcode/703-Kth-Largest-Element-in-a-Stream.cpp
@@ -0,0 +1,31 @@
+#include <queue>
+class KthLargest {
+    std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
+    int k;
+
+public:
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        for (int i = 0; i < nums.size(); i++) {
+            minHeap.push(nums[i]);
+
+            if (minHeap.size() > k) {
+                minHeap.pop();
+            }
+        }
+    }
+
+    int add(int val) {
+        minHeap.push(val);
+        if (minHeap.size() > k) {
+            minHeap.pop();
+        }
+        return minHeap.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
