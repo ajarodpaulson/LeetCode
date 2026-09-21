d9b6a72e29565b95df7956eab9c739c0b1f2490e
Jarod Paulson <ajarodpaulson.ubc@gmail.com>
2026-09-21 10:08:04 -0700

    [747] [Time Beats: 100.00%] [Memory Beats: 52.90%] - LeetPush

--- Leetcode/747-Largest-Number-At-Least-Twice-of-Others.py
+++ Leetcode/747-Largest-Number-At-Least-Twice-of-Others.py
@@ -1,16 +1,17 @@
 class Solution:
     def dominantIndex(self, nums: list[int]) -> int:
-        largest = float(-inf)
-        second_largest = float(-inf)
-        largest_idx = None
+        largest = [float(-inf), float(-inf)] # value, idx
+        second_largest = [float(-inf), float(-inf)]
+
         for idx, num in enumerate(nums):
-            if num > largest:
-                second_largest = largest
-                largest = num
-                largest_idx = idx
-            elif num > second_largest:
-                second_largest = num
-        if second_largest * 2 <= largest:
-            return largest_idx
-        else:
-            return -1
+            if num > largest[0]:
+                second_largest[0] = largest[0]
+                second_largest[1] = largest[1]
+                largest[0] = num
+                largest[1] = idx
+            elif num > second_largest[0]:
+                second_largest[0] = num
+                second_largest[1] = idx
+
+        return largest[1] if second_largest[0] * 2 <= largest[0] else -1
+        
